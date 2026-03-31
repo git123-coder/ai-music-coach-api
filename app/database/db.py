e5f8e23e7d42e2f41a56e28ff5bf78de694d6c37
Vinay Lakshman <vinaylakshman@Vinays-MacBook-Air.local>
2026-03-31 19:50:05 +0530

    Minor corrections

--- app/database/db.py
+++ app/database/db.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL,
-    connect_args={"check_same_thread": False}  
+    connect_args={"check_same_thread": False})  
 
 # connect_args required for SQLite, remove it if using PostgreSQL.
 
