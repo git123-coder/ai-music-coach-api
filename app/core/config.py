f1cc20151f089a071046b648561fc2d9051ff9ce
Vinay Lakshman <vinaylakshman@Vinays-MacBook-Air.local>
2026-03-31 19:41:14 +0530

    removing postgres and using sqlite for deployment

--- app/core/config.py
+++ app/core/config.py
@@ -3,9 +3,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+# DATABASE_URL = os.getenv("DATABASE_URL")
+# SECRET_KEY = os.getenv("SECRET_KEY", "supersecret")
 
-SECRET_KEY = os.getenv("SECRET_KEY", "supersecret")
+# commented out above line and added below line to use sqlite for local development, 
+# uncomment above line and set DATABASE_URL in .env to deploy using postgres.
+
+DATABASE_URL = "sqlite:///./music_coach.db"
+
 
 ALGORITHM = "HS256"
 
--- app/database/db.py
+++ app/database/db.py
@@ -2,7 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL,
+    connect_args={"check_same_thread": False}  
+
+# connect_args required for SQLite, remove it if using PostgreSQL.
 
 SessionLocal = sessionmaker(
     autocommit=False,
